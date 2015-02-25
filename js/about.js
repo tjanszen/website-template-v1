3ff338f425ce9c5d74e2c8bcfcedfbc9a6a61e98
Tommy Janszen <tommy.janszen@gmail.com>
2015-02-25 09:09:46 -0800

    add lorem text and update links for templare

--- js/about.js
+++ js/about.js
@@ -1,16 +1,16 @@
 var lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br><br> Hic, fugit, corporis. Consequuntur officiis illum dignissimos.<br><br> Commodi architecto iste, ipsam sequi amet perferendis dolorum nesciunt, corporis explicabo, aspernatur porro molestiae doloremque."
 
-var learner = "Advantageous about learning something new every single day. Whether it is listing to an iTunes podcast, watching a YouTube clip, reading a book, or reading through blogs - I always search out ways to broaden my perspective.<br><br> One of my favorite quotes reads &ldquo;Life is not about finding yourself, life is about creating yourself&rdquo;. Creating is about searching out experiences to learn and grow."
-var programming = "I am autodidact who has the passion and determination to pursue his dream as Full Stack Developer. <br><br>One of my side projects is developing a site to help guide those who are passionate about learning to code. There thousands of resources available online but choosing what to learn can be difficult. Having a plan on how to get somewhere is always a good idea. The site will help guide those interested in taking their coding skills to the next level."
-var NCAA = "Growing up I always aspired to swim in college. I walked-on Miami University&#39;s Swim Team. I earned a spot on the team, received an athletic scholarship after my first year performances, and competed four years as a NCAA Division I Athlete.<br><br> Swimming taught me the importance failure. I failed hundreds of times during races, practice, and competition. Life is about picking yourself up and learning from your experiences."
-var business = "My business degree is in Marketing. I am a true believer in companies and individuals establishing unique brands. Brands distinguish us from one another and allow us to establish unique identities. Developing a voice and message empower companies and individuals.<br><br> In addition to my studies, working for a startup company was one of the best decisions in my career. Startups force you to think smart, be creative in tough situations, take ownership, and learn to overcome adversity. "
-var experience = "I have been fortunate to work with great brands throughout my career.<br><br> My first PM projects were for P&G brands Pringles, Downy, Puffs, and Pantene. While consulting at eyewear giant Luxottica Retail, I was the PM for the Sunglass Hut Australia e-commerce website (desktop and mobile). Recently at LISNR (Cincinnati startup) I was the PM for the Budweiser Made In America mobile app (iOS and Android) and a mobile application for Walmart (iOS and Android)."
+var ID1 = "Lorem1111 ipsum dolor sit amet, consectetur adipisicing elit. <br><br> Hic, fugit, corporis. Consequuntur officiis illum dignissimos.<br><br> Commodi architecto iste, ipsam sequi amet perferendis dolorum nesciunt, corporis explicabo, aspernatur porro molestiae doloremque."
+var ID2 = "Lorem2222 ipsum dolor sit amet, consectetur adipisicing elit. <br><br> Hic, fugit, corporis. Consequuntur officiis illum dignissimos.<br><br> Commodi architecto iste, ipsam sequi amet perferendis dolorum nesciunt, corporis explicabo, aspernatur porro molestiae doloremque."
+var ID3 = "Lorem3333 ipsum dolor sit amet, consectetur adipisicing elit. <br><br> Hic, fugit, corporis. Consequuntur officiis illum dignissimos.<br><br> Commodi architecto iste, ipsam sequi amet perferendis dolorum nesciunt, corporis explicabo, aspernatur porro molestiae doloremque."
+var ID4 = "Lorem4444 ipsum dolor sit amet, consectetur adipisicing elit. <br><br> Hic, fugit, corporis. Consequuntur officiis illum dignissimos.<br><br> Commodi architecto iste, ipsam sequi amet perferendis dolorum nesciunt, corporis explicabo, aspernatur porro molestiae doloremque."
+var ID5 = "Lorem5555 ipsum dolor sit amet, consectetur adipisicing elit. <br><br> Hic, fugit, corporis. Consequuntur officiis illum dignissimos.<br><br> Commodi architecto iste, ipsam sequi amet perferendis dolorum nesciunt, corporis explicabo, aspernatur porro molestiae doloremque."
 
-var array = [["learner", learner],
-			 ["programming", programming],
-			 ["NCAA", NCAA],
-			  ["business", business],
-			  ["experience", experience]
+var array = [["ID1", ID1],
+			 ["ID2", ID2],
+			 ["ID3", ID3],
+			  ["ID4", ID4],
+			  ["ID5", ID5]
 			   ];
 window.onload = init;
 
@@ -44,7 +44,7 @@ function slideInfo(e){
 			aboutRightText.innerHTML = array[i][1];
 		}
 	};
-	
+
 }
 
 function expand(e){
@@ -57,19 +57,3 @@ function expand(e){
 		li.setAttribute("class", "ind-books ind-books-closed");
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
